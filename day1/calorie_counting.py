9fb682601898ec5d883769855acee734741d0a59
unix <edgarzubel@gmail.com>
2022-12-01 20:58:48 +0200

    Wrote solution for second task

--- day1/calorie_counting.py
+++ day1/calorie_counting.py
@@ -2,6 +2,8 @@ import numpy as np
 
 arr = []
 sum = 0
+
+#Part 1
 with open("input.txt") as file:
     for line in file:
         if line.strip():
@@ -11,5 +13,12 @@ with open("input.txt") as file:
             sum = 0
 
 arr = np.array(arr)
-arr = np.max(arr)
-print(arr)
+arr_max = np.max(arr)
+print(arr_max)
+
+#Part 2
+top3 = 0
+ind = np.argsort(arr)[::-1][:3]
+for i in ind:
+    top3 += arr[i]
+print(top3)
